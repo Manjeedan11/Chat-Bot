8adc5bbb45431679556cddb58a181e7df42ca018
unknown <shakthivelmanjeedan11@gmail.com>
2024-05-10 19:05:26 +0530

    Fixed a few bugs in train.py and included a logic to save the train model

--- train.py
+++ train.py
@@ -1,20 +1,20 @@
-import json 
+import json
 from nltk_utils import tokenize, stem, bag_of_words
-import numpy as np 
-
+import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader 
+from torch.utils.data import Dataset, DataLoader
+from model import NeuraNet
+
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
 
-#print(intents)
 
 all_words = []
 tags = []
 xy = []
-for intent in intents ['intents']:
+for intent in intents['intents']:
     tag = intent['tags']
     tags.append(tag)
     for pattern in intent['patterns']:
@@ -23,38 +23,92 @@ for intent in intents ['intents']:
         xy.append((w, tag))
 
 
-ignore_words =['?', '!', '.', ',']
+ignore_words = ['?', '!', '.', ',']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print(tags)
+
 
 X_train = []
 y_train = []
 for (pattern_sentence, tag) in xy:
-    bag = bags_of_words(pattern_sentence, all_words) 
+    bag = bag_of_words(pattern_sentence, all_words)
     X_train.append(bag)
-
     label = tags.index(tag)
-    y_train.append(label) #CrossEntropyLoss
+    y_train.append(label)
 
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
+
 class ChatDataset(Dataset):
     def __init__(self):
-        self.n.samples = len(X_train)
+        self.n_samples = len(X_train)
         self.x_data = X_train
         self.y_data = y_train
 
     def __getitem__(self, index):
-        return self.x_data[idx], self.y_data[idx]
+        return self.x_data[index], self.y_data[index]
 
     def __len__(self):
-        return self.n.samples
+        return self.n_samples
+
 
 batch_size = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(X_train[0])
+learning_rate = 0.001
+num_epochs = 1000
 
-dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+model = NeuraNet(input_size, hidden_size, output_size)
+
+
+def train():
+    dataset = ChatDataset()
+    train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(num_epochs):
+        for words, labels in train_loader:
+            words = words.to(device)
+            labels = labels.to(device).long()
+
+            outputs = model(words)
+            loss = criterion(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        if (epoch + 1) % 100 == 0:
+            print(f'epoch {epoch + 1}/{num_epochs}, loss={loss.item():.4f}')
+
+    print(f'final loss, loss={loss.item():.4f}')
+
+
+if __name__ == '__main__':
+    torch.multiprocessing.freeze_support()
+    train()
+
+
+data = {
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "output_size": output_size,
+    "hidden_size": hidden_size,
+    "all_words": all_words,
+    "tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to :{FILE}')
