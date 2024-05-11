7cfa3ddf89e2324aedc6143aace66a8e07e90958
unknown <shakthivelmanjeedan11@gmail.com>
2024-05-11 12:50:11 +0530

    Fixed a few bugs in app.py ensuring the probs logic works

--- app.py
+++ app.py
@@ -1,58 +1,63 @@
-from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask import Flask, request, jsonify
+import random
 import json
-import numpy as np
 import torch
 from model import NeuraNet
-from nltk_utils import tokenize, stem, bag_of_words
+from nltk_utils import bag_of_words, tokenize
 
 app = Flask(__name__)
 CORS(app)
 
-# Load intents and model data
-with open('intents.json', 'r') as f:
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open("intents.json", "r") as f:
     intents = json.load(f)
 
 FILE = "data.pth"
 data = torch.load(FILE)
 
 input_size = data["input_size"]
-output_size = data["output_size"]
 hidden_size = data["hidden_size"]
-all_words = data['all_words']
-tags = data['tags']
+output_size = data["output_size"]
+all_words = data["all_words"]
+tags = data["tags"]
 model_state = data["model_state"]
 
-model = NeuraNet(input_size, hidden_size, output_size)
+model = NeuraNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-# Function to predict tag
-def predict_tag(sentence):
-    X = bag_of_words(tokenize(sentence), all_words)
+bot_name = "FATHIMA"
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    message = data['message']
+    response = get_response(message)
+    return jsonify({'response': response})
+
+def get_response(message):
+    sentence = tokenize(message)
+    X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X)
 
     output = model(X)
     _, predicted = torch.max(output, dim=1)
-
     tag = tags[predicted.item()]
 
-    return tag
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
 
-# Route for chatbot
-@app.route('/chat', methods=['POST'])
-def chat():
-    data = request.get_json()
-    message = data['message']
-    tags = predict_tag(message)
-
-    for intent in intents['intents']:
-        if intent['tags'] == tags:
-            response = np.random.choice(intent['responses'])
-            break
-
-    return jsonify({'response': response})
+    if prob.item() > 0.75:
+        for intent in intents["intents"]:
+            if tag == intent["tags"]:
+                return random.choice(intent['responses'])
+    else:
+        return "I do not understand..."
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
+
+
